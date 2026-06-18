73310129563b8b2be809f0532c7bc0fdd9f9d79e
whotterre <ijeddy5.0@gmail.com>
2026-06-18 15:38:26 +0100

    feat: implement process method for validation stage

--- 03-http-middleware/06-interface-based-middleware-chain/stages/validation.go
+++ 03-http-middleware/06-interface-based-middleware-chain/stages/validation.go
@@ -2,13 +2,16 @@ package stages
 
 import (
 	"context"
+	"errors"
+	"time"
+
 	"github.com/medunes/go-kata/03-http-middleware/06-interface-based-middleware-chain/common"
 	"github.com/medunes/go-kata/03-http-middleware/06-interface-based-middleware-chain/metrics"
 )
 
 // Validation stage (First stage)
 type ValidationProcessor struct {
-	next common.Processor
+	next    common.Processor
 	metrics metrics.MetricsCollector
 }
 
@@ -24,8 +27,29 @@ func NewValidatorProcessor(opts ...ValidationOption) common.Processor {
 	return processor
 }
 
-func (p *ValidationProcessor) Process(context.Context, common.Event) ([]common.Event, error) {
-	return []common.Event{}, nil
+func (p *ValidationProcessor) Process(ctx context.Context, ev common.Event) ([]common.Event, error) {
+	start := time.Now()
+	defer func() {
+		if p.metrics != nil {
+			p.metrics.MeasureLatency("validation", time.Since(start))
+		}
+	}()
+	if err := ctx.Err(); err != nil {
+		return nil, err
+	}
+
+	if ev.ID == "" {
+		return nil, errors.New("Event with invalid event ID passed validation stage")
+	}
+
+	var res []common.Event
+
+	res = append(res, ev)
+
+	if p.metrics != nil {
+		p.metrics.CountEvent("validation")
+	}
+	return p.next.Process(ctx, ev)
 }
 
 func WithValidationMetricsCollector(collector metrics.MetricsCollector) ValidationOption {
