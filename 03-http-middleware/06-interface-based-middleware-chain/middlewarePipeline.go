2eacf16a0f5e88c594311983a2e87c6325b20c0b
whotterre <ijeddy5.0@gmail.com>
2026-06-18 14:20:07 +0100

    feat: implement basic metric collector for middleware pipeline

--- 03-http-middleware/06-interface-based-middleware-chain/middlewarePipeline.go
+++ 03-http-middleware/06-interface-based-middleware-chain/middlewarePipeline.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"log" // replace with slog in a later impl (all hail the odius Procrastinator...inator)
+	"sync"
 	"time"
 )
 
@@ -9,13 +11,46 @@ type Event struct {
 	ID   string
 	Type string
 }
+
+
+// do we persist collected metrics?
+type MetricsCollector interface {
+    CountEvent(stage string)
+    MeasureLatency(stage string, duration time.Duration)
+}
+
+type metricsCollector struct {
+	logger *log.Logger
+	mu sync.Mutex
+	counts map[string]int
+}
+
+func (c *metricsCollector) CountEvent(stage string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.counts[stage]++ 
+
+	c.logger.Printf("[Metrics] Stage %s processed an event. Total: %d", stage, c.counts[stage])
+}
+
+func (c *metricsCollector) MeasureLatency(stage string, duration time.Duration) {
+	c.logger.Printf("Stage: %s | Latency: %v", stage, duration)
+}
+
+func WithMetricsCollector() MetricsCollector {
+	return &metricsCollector{}
+}
+
 // Validation, enrichment, filtering, and finally storage.
-// track metrics at each stage)
-// how do we achieve recoverablitity
+// track metrics at each stage
+// how do we achieve recoverability?
+
 type Processor interface {
 	Process(context.Context, Event) ([]Event, error)
 }
 
+// Validation stage 
 type ValidationProcessor struct {
 	next Processor
 }
@@ -36,6 +71,7 @@ func (p *ValidationProcessor) Process(context.Context, Event) ([]Event, error) {
 	return []Event{}, nil
 }
 
+// Enrichment stage
 
 type EnrichmentOption func(*EnrichmentProcessor)
 
@@ -59,6 +95,8 @@ func (p *EnrichmentProcessor) Process(context.Context, Event) ([]Event, error) {
 	return []Event{}, nil
 }
 
+
+// Filtering stage 
 type FilteringProcessor struct {
 	next Processor
 }
@@ -79,6 +117,7 @@ func NewFilteringProcessor(opts ...FilteringOption) Processor {
 	return processor
 }
 
+// Storage stage 
 type StorageOption func(*StorageProcessor)
 
 type StorageProcessor struct {}
@@ -98,10 +137,6 @@ func NewStorageProcessor(opts ...StorageOption) Processor {
 }
 
 
-
-
-
-
 func main() {
-
+	println("Docete me et ergo ipse yada yada...")
 }
